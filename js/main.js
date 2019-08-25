f93fd0b065612756f1a0d431a4024040b0e93d6a
Stefan Schraberger <schrabergerstefan@gmail.com>
2019-08-26 00:14:02 +0200

    Search function added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+// Lightbox config
+
 lightbox.option({
   fitImagesInViewport: true
+});
+
+// Search function
+
+const search = document.querySelector(".search");
+search.addEventListener("keyup", function(event) {
+  const searchTerm = event.target.value.toLowerCase();
+  const photos = document.querySelectorAll(".photo");
+  const links = document.querySelectorAll(".photo a");
+  const titles = [];
+  for (let i = 0; i < links.length; i++) {
+    titles.push(links[i].getAttribute("data-title").toLowerCase());
+  }
+  for (let i = 0; i < titles.length; i++) {
+    if (titles[i].indexOf(searchTerm) > -1) {
+      photos[i].style.display = "block";
+    } else {
+      photos[i].style.display = "none";
+    }
+  }
 });
